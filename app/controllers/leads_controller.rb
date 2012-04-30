66fef9ae1b85367375fb495e81a637cdefd6bbbc
Casper Fabricius <me@casperfabricius.com>
2012-04-30 10:30:49 +0200

    Added updating of Lead items

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,20 +1,34 @@
 class LeadsController < ApplicationController
-  
+  before_filter :load_collections, :only => [:new, :edit]
+
   def index
     @leads = Lead.all
   end
-  
+
   def new
     @lead = Lead.new
-    
-    @lead_contacts = Lead.space_contacts
-    @sales_contacts = Lead.users
-    @statuses = Lead.statuses
   end
-  
+
   def create
     Lead.create_from_params(params['lead'])
     redirect_to leads_path, :notice => 'Lead created'
   end
-  
+
+  def edit
+    @lead = Lead.find_basic(params[:id])
+  end
+
+  def update
+    Lead.update_from_params(params[:id], params['lead'])
+    redirect_to leads_path, :notice => 'Lead updated'
+  end
+
+  protected
+
+    def load_collections
+      @lead_contacts = Lead.space_contacts
+      @sales_contacts = Lead.users
+      @statuses = Lead.statuses
+    end
+
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,64 +1,63 @@
 class Lead < Podio::Item
   APP_ID = 573839
   SPACE_ID = 153322
-  
+
   # Find all items in the Leads app
   def self.all
     collection = self.find_all(APP_ID)
     collection[:all]
   end
-  
+
   # Find valid lead contacts in the space
   def self.space_contacts
     Podio::Contact.find_all_for_space(SPACE_ID, :order => 'contact', :limit => 12, :contact_type => 'space,connection', :exclude_self => false)
   end
-  
+
   # Find valid sales contacts in the space
   def self.users
     Podio::Contact.find_all_for_space(SPACE_ID, :order => 'contact', :limit => 12, :contact_type => 'user', :exclude_self => false)
   end
-  
+
   # Find valid statuses
   def self.statuses
     app = Podio::Application.find(APP_ID)
     field = app.fields.find { |field| field['external_id'] == 'status' }
     field['config']['settings']['allowed_values']
   end
-  
+
   def self.create_from_params(params)
-    fields = {
-      'company-or-organisation' => params[:organization],
-      'contacts' => params[:lead_contact].to_i,
-      'sales-contact' => params[:sales_contact].to_i,
-      'potential-revenue' => { :value => params['potential_revenue'], :currency => 'USD' },
-      'probability-of-sale' => params[:probability].to_i,
-      'status' => params[:status],
-      'next-follow-up' => DateTime.new(params['followup_at(1i)'].to_i, params['followup_at(2i)'].to_i, params['followup_at(3i)'].to_i).to_s(:db)
-    }
     # raise fields.inspect
-    self.create(APP_ID, { :fields => fields })
+    self.create(APP_ID, { :fields => fields_from_params(params) })
   end
-  
+
+  def self.update_from_params(id, params)
+    self.update(id, { :fields => fields_from_params(params) })
+  end
+
   #
   # Map the field values return by the Podio API to simple getters
   #
-  
+
   def organization
     field_values_by_external_id('company-or-organisation', :simple => true)
   end
-  
+
   def lead_contact
     field_values_by_external_id('contacts', :simple => true).try(:[], 'name')
   end
-  
+
   def sales_contact
     field_values_by_external_id('sales-contact', :simple => true).try(:[], 'name')
   end
-  
-  def potential_revenue
-    field_values_by_external_id('potential-revenue').try(:first)
+
+  def potential_revenue_value
+    field_values_by_external_id('potential-revenue').try(:first).try(:[], 'value').to_i
   end
-  
+
+  def potential_revenue_currency
+    field_values_by_external_id('potential-revenue').try(:first).try(:[], 'currency')
+  end
+
   def probability
     field_values_by_external_id('probability-of-sale', :simple => true)
   end
@@ -66,13 +65,13 @@ class Lead < Podio::Item
   def status
     field_values_by_external_id('status', :simple => true)
   end
-  
+
   def followup_at
-    field_values_by_external_id('next-follow-up').try(:first).try(:[], 'start')
+    field_values_by_external_id('next-follow-up').try(:first).try(:[], 'start').try(:to_datetime)
   end
-  
+
   protected
-  
+
     def field_values_by_external_id(external_id, options = {})
       if self.fields.present?
         values = self.fields.find { |field| field['external_id'] == external_id }['values']
@@ -85,5 +84,17 @@ class Lead < Podio::Item
         nil
       end
     end
-  
+
+    def self.fields_from_params(params)
+      {
+        'company-or-organisation' => params[:organization],
+        'contacts' => params[:lead_contact].to_i,
+        'sales-contact' => params[:sales_contact].to_i,
+        'potential-revenue' => { :value => params['potential_revenue_value'], :currency => params['potential_revenue_currency'] },
+        'probability-of-sale' => params[:probability].to_i,
+        'status' => params[:status],
+        'next-follow-up' => DateTime.new(params['followup_at(1i)'].to_i, params['followup_at(2i)'].to_i, params['followup_at(3i)'].to_i).to_s(:db)
+      }
+    end
+
 end
